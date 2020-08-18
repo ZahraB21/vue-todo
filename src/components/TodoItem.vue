d18bbe442478ec8ef8119fc42e3c636c194aa8fd
Jaazbe <jaazbeh@gmail.com>
2020-08-17 20:18:32 -0700

    TodoItem component created and added

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>{{todo.title}}</p>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,14 +2,17 @@
   <div>
     <h1>Todos App</h1>
     <div v-for="todo in todos" v-bind:key="todo.id">
-      <p>{{todo.title}}</p>
+      <TodoItem />
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
+
 export default {
   name: "Todos",
+  components: { TodoItem },
   props: ["todos"],
 };
 </script>
